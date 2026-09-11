079ab3d694c65c4be2516745c9d081c3a55f43b7
dodoBrb <dorine.barbey.auditeur@lecnam.net>
2026-09-11 13:50:06 +0200

    Exercice 9 (étudiant)

---
+++ src/Exercice09/Main.java
@@ -0,0 +1,22 @@
+package Exercice09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Etudiant> promo = new ArrayList<>();
+
+        Etudiant e1 = new Etudiant("E001", "Dorine", 15.5);
+        Etudiant e2 = new Etudiant("E002", "Juliette", 12.0);
+
+        promo.add(e1);
+        promo.add(e2);
+
+        Etudiant e1Copie = new Etudiant("E001", "Dorine (doublon)", 8.0);
+
+        System.out.println("e1.equals(e1Copie) : " + e1.equals(e1Copie));
+
+        System.out.println("La liste contient-elle e1Copie ? " + promo.contains(e1Copie));
+    }
+}
