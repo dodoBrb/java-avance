98d998b01ea68e7162969acaf66b984461298a12
dodoBrb <dorine.barbey.auditeur@lecnam.net>
2026-09-11 10:31:24 +0200

    Exercice 1 (livre)

---
+++ src/Livre.java
@@ -0,0 +1,27 @@
+public class Livre {
+    private String titre;
+    private String auteur;
+    private int nombrePages;
+    private boolean disponible;
+
+    public Livre(String titre, String auteur, int nombrePages) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.nombrePages = nombrePages;
+        this.disponible = true;
+    }
+
+    public void emprunter() {
+        if (this.disponible) {
+            this.disponible = false;
+            System.out.println("Le livre " + this.titre + " a été emprunté.");
+        } else {
+            System.out.println("Le livre " + this.titre + " n'est pas disponible pour le moment.");
+        }
+    }
+
+    public void rendre() {
+        this.disponible = true;
+        System.out.println("Le livre " + this.titre + " a été rendu.");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        Livre livre1 = new Livre("Le Comte de Monte-Cristo", "Alexandre Dumas", 1234);
+        Livre livre2 = new Livre("Harry Potter", "J.K. Rolling", 1478);
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        livre1.emprunter();
+        livre1.emprunter();
+
+
+        livre1.rendre();
+        livre1.emprunter();
+
+        livre2.emprunter();
     }
 }
